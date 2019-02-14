a344b84ea30659a5a42f6269246d9904b87b9a91
greguu <greguu@null.net>
2019-02-14 18:28:14 +1300

    Update armv5tel-musl.sh

--- common/cross-profiles/armv5tel-musl.sh
+++ common/cross-profiles/armv5tel-musl.sh
@@ -3,7 +3,7 @@
 XBPS_TARGET_MACHINE="armv5tel-musl"
 XBPS_TARGET_QEMU_MACHINE="arm"
 XBPS_CROSS_TRIPLET="arm-linux-musleabi"
-XBPS_CROSS_CFLAGS="-march=armv5te -msoft-float -mfloat-abi=soft"
+XBPS_CROSS_CFLAGS="-march=armv5te -mtune=xscale -msoft-float -mfloat-abi=soft"
 XBPS_CROSS_CXXFLAGS="$XBPS_CROSS_CFLAGS"
 XBPS_CROSS_FFLAGS="$XBPS_CROSS_CFLAGS"
 XBPS_CROSS_RUSTFLAGS="--sysroot=${XBPS_CROSS_BASE}/usr"
